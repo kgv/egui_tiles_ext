1bde8798429b3d7d52865a6bf49e79b15a1a7596
kgv <kgv@users.noreply.github.com>
2024-12-17 14:05:21 +0300

    0.0.2

--- src/tiles.rs
+++ src/tiles.rs
@@ -3,6 +3,14 @@ use egui_tiles::{Tile, TileId, Tiles};
 /// Extension methods for [`Tiles`]
 pub trait TilesExt<T> {
     fn find_pane_by(&mut self, f: impl Fn(&T) -> bool) -> Option<TileId>;
+
+    fn panes<'a>(&'a self) -> impl Iterator<Item = &'a T>
+    where
+        T: 'a;
+
+    fn panes_mut<'a>(&'a mut self) -> impl Iterator<Item = &'a mut T>
+    where
+        T: 'a;
 }
 
 impl<T> TilesExt<T> for Tiles<T> {
@@ -16,5 +24,19 @@ impl<T> TilesExt<T> for Tiles<T> {
                 }
             })
             .map(|(tile_id, _)| *tile_id)
+    }
+
+    fn panes<'a>(&self) -> impl Iterator<Item = &T> {
+        self.tiles().filter_map(|tile| match tile {
+            Tile::Pane(pane) => Some(pane),
+            Tile::Container(_) => None,
+        })
+    }
+
+    fn panes_mut<'a>(&mut self) -> impl Iterator<Item = &mut T> {
+        self.tiles_mut().filter_map(|tile| match tile {
+            Tile::Pane(pane) => Some(pane),
+            Tile::Container(_) => None,
+        })
     }
 }
